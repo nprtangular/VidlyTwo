cfc36d9199fc66895e58e8ef4e0192ae51755fe1
nprtangular <nprtangular@gmail.com>
2018-10-26 01:28:40 -0500

    Agregar archivos de proyecto.

---
+++ VidlyTwo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VidlyTwo.MvcApplication" Language="C#" %>
---
+++ VidlyTwo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidlyTwo.Startup))]
+namespace VidlyTwo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
